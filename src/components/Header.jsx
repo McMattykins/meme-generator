ec17bd980aec6ee4d18e227e744809ad48bb96bb
McMattykkins <matt.askett+github@gmail.com>
2023-01-24 23:21:28 +0900

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="#" alt="Picture of the Troll Face" className="header--image" />
+      <h1 className="header--title">Meme Generator</h1>
+    </header>
+  )
+}
